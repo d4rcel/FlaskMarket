810ad72638d4f021aa695728419e567e2095ded9
d4rcel <daraola05@gmail.com>
2024-04-26 10:24:23 +0100

    User uthentification

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 
 @app.route("/")
 @app.route('/home')
@@ -19,7 +19,7 @@ def market_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password_hash=form.password1.data)
+        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -28,3 +28,9 @@ def register_page():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
